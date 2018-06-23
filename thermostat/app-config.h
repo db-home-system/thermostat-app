571a566ece323095612393912385f3f3f8360de7
Daniele Basile <asterix24@gmail.com>
2018-06-24 00:15:39 +0200

    Reformat.

--- thermostat/app-config.h
+++ thermostat/app-config.h
@@ -2,14 +2,15 @@
 #define APPCONFIG_H
 
 // Timeline cfg
-#define TIMELINE_DIVISION 24
+#define TIMELINE_DIVISION       24
 
 //Settings and ENV
 #define APP_SETTINGS_ROOT      "/etc/therapp"
-#define TIMELINE_CFG           "timeline.cfg";
+#define TIMELINE_CFG           "timeline.cfg"
 
 #define APP_SETTINGS_ROOT_ENV  "THERAPP_SETTINGS_ROOT"
 #define TIMELINE_CFG_ENV       "THERAPP_TIMELINE_CFG"
 
+#define APP_TEMPERATURE_FILE   "/tmp/temperature"
 
 #endif // APPCONFIG_H
